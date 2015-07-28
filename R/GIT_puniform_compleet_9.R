1b1331cb17a7065df548fd468b98a6860b3a9342
RobbievanAert <R.C.M.vanAert@tilburguniversity.edu>
2015-07-28 16:30:56 +0200

    Changes in documentation

--- R/GIT_puniform_compleet_9.R
+++ R/GIT_puniform_compleet_9.R
@@ -1,6 +1,6 @@
 #' p-uniform
 #'
-#' Function to apply p-uniform method for one-sample means, two-sample means, and raw correlation coefficients. \cr
+#' Function to apply p-uniform method for one-sample mean, two-independent means, and one raw correlation coefficient as described in van Assen, van Aert, and Wicherts (2014) and van Aert, Wicherts, and van Assen (2015). \cr
 #' \cr
 #' Please note that the method is still in development and that this is a beta version. If you suspect a bug, please send me an email (\email{R.C.M.vanAert@@tilburguniversity.edu}).
 #'
@@ -8,27 +8,32 @@
 #' @param ri A vector of raw correlations
 #' @param ni A vector of sample sizes for one-sample means and correlations
 #' @param sdi A vector of standard deviations for one-sample means
-#' @param m1i A vector of means in group 1 for two-sample means
-#' @param m2i A vector of means in group 2 for two-sample means
-#' @param n1i A vector of sample sizes in group 1 for two-sample means
-#' @param n2i A vector of sample sizes in group 2 for two-sample means
-#' @param sd1i A vector of standard deviations in group 1 for two-sample means
-#' @param sd2i A vector of standard deviations in group 2 for two-sample means
+#' @param m1i A vector of means in group 1 for two-independent means
+#' @param m2i A vector of means in group 2 for two-independent means
+#' @param n1i A vector of sample sizes in group 1 for two-independent means
+#' @param n2i A vector of sample sizes in group 2 for two-independent means
+#' @param sd1i A vector of standard deviations in group 1 for two-independent means
+#' @param sd2i A vector of standard deviations in group 2 for two-independent means
 #' @param tobs A vector of t-values
 #' @param alpha A integer specifying the alpha level as used in primary studies (default is 0.05)
-#' @param side A character indicating the direction of the tested hypothesis in the primary studies (either \code{"right"} or \code{"left"})
+#' @param side A character indicating whether the effect sizes in the primary studies are in the right-tail of the distribution (i.e., positive)  or in the left-tail of the distribution (i.e., negative) (either \code{"right"} or \code{"left"})
 #' @param method A character indicating the method to be used (\code{"P"} (default), \code{"LNP"}, \code{"LN1MINP"}, \code{"KS"}, or \code{"AD"})
 #' @param plot A logical indicating whether a plot showing the relation between observed and expected p-values has to be rendered (default is \code{TRUE})
 #'
-#' @details Three different effect sizes can be used as input for the \code{puniform} function: one-sample means, two-sample means, and raw correlation coefficients. Analyzing one-sample
-#' means and two-sample means can be done by either providing the function group means (\code{mi} or \code{m1i} and \code{m2i}), standard deviations (\code{sdi} or \code{sd1i} and \code{sd2i}),
-#' and sample sizes (\code{ni} or \code{n1i} and \code{n2i}) or t-values (\code{tobs}) and sample sizes (\code{ni} or \code{n1i} and \code{n2i}). Both options should be accompanied with input for
-#' the arguments \code{side} and \code{method}. See the Example section for examples.
+#' @details Three different effect sizes can be used as input for the \code{puniform} function: one-sample means, two-independent means, and raw correlation coefficients.
+#' Analyzing one-sample means and two-independent means can be done by either providing the function group means (\code{mi} or \code{m1i} and \code{m2i}), standard deviations
+#' (\code{sdi} or \code{sd1i} and \code{sd2i}), and sample sizes (\code{ni} or \code{n1i} and \code{n2i}) or t-values (\code{tobs}) and sample sizes (\code{ni} or \code{n1i}
+#' and \code{n2i}). Both options should be accompanied with input for the arguments \code{side} and \code{method}. See the Example section for examples. Raw correlation
+#' coefficients can be analyzed by supplying \code{ri} and \code{ni} to the \code{puniform} function next to input for the arguments \code{side} and \code{method}.
 #'
-#' Raw correlation coefficients can be analyzed by supplying \code{ri} and \code{ni} to the \code{puniform} function next to input for the arguments \code{side} and \code{method}.
+#' Note that only one effect size measure can be specified at a time. A combination of effect size measures usually causes true heterogeneity among effect sizes and including
+#' different effect size measures is therefore not recommended.
 #'
-#' Note that only one effect size measure can be specified at a time. A combination of effect size measures usually causes true heterogeneity among effect sizes and including different effect size measures
-#' is therefore not recommended.
+#' Five different estimators can be used when applying p-uniform. The \code{P} method is based on the distribution of the sum of independent uniformly distributed random
+#' variables (Irwin-Hall distribution) and is the recommended estimator (van Aert et al., 2015). The \code{LNP} estimator refers to Fisher’s method (1950, Chapter 4)
+#' for combining p-values and the \code{LN1MINP} estimator first computes 1 – p-value in each study before applying Fisher’s method on these transformed p-values
+#' (van Assen et al., 2014). \code{KS} and \code{AD} respectively use the Kolmogorov-Smirnov test (Massey, 1951) and the Anderson-Darling test (Anderson & Darling, 1954)
+#' for testing whether the (conditional) p-values follow a uniform distribution.
 #'
 #' @return
 #' \item{est}{p-uniform's effect size estimate}
@@ -50,11 +55,11 @@
 #'
 #' @author Robbie C.M. van Aert \email{R.C.M.vanAert@@tilburguniversity.edu}
 #'
-#' @note The p-uniform method is described in: \cr
-#' \cr
-#' van Assen, M. A. L. M., van Aert, R. C. M., & Wicherts, J. M. (2014).
-#' Meta-analysis using effect size distributions of only statistically significant studies. Psychological Methods,
-#' Advance online publication. doi: http://dx.doi.org/10.1037/met0000025
+#' @references Anderson, T. W., & Darling, D. A. (1954). A test of goodness of fit. Journal of the American Statistical Association, 49(268), 765-769.
+#' @references Fisher, R. A. (1950). Statistical methods for research workers (11th ed.). London: Oliver & Boyd.
+#' @references Massey, F. J. (1951). The Kolmogorov-Smirnov test for goodness of fit. Journal of the American Statistical Association, 46(253), 68-78.
+#' @references van Aert, R. C. M., Wicherts, J. M., & van Assen, M. A. L. M. (2015). Conducting meta-analyses on p-values: Reservations and recommendations for applying p-uniform and p-curve. Manuscript in preparation.
+#' @references van Assen, M. A. L. M., van Aert, R. C. M., & Wicherts, J. M. (2014). Meta-analysis using effect size distributions of only statistically significant studies. Psychological Methods, Advance online publication. doi: http://dx.doi.org/10.1037/met0000025
 #'
 #' @examples ### Load data from meta-analysis by McCall and Carriger (1993)
 #' data(data.mccall93)
