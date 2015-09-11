c097e662588c253ecc925a356ace672a84cde9ec
RobbievanAert <R.C.M.vanAert@tilburguniversity.edu>
2015-09-11 13:25:54 +0200

    p <.001

--- R/GIT_print.puniformoutput_2.R
+++ R/GIT_print.puniformoutput_2.R
@@ -19,7 +19,8 @@ print.puniformoutput <- function(x) {
   cat("\n")
   cat("\n")
   if (x$method == "LNP" | x$method == "LN1MINP" | x$method == "P") {
-    print(format(data.frame(est = round(x$est, 4), ci.lb = round(x$ci.lb, 4), ci.ub = round(x$ci.ub, 4), L.0 = round(x$L.0, 4), pval = round(x$pval.0, 4), ksig = x$ksig, row.names = ""), width = 9))
+    x$pval.0 <- ifelse(x$pval.0 < .001, "  <.001", round(x$pval.0, 4))
+    print(format(data.frame(est = round(x$est, 4), ci.lb = round(x$ci.lb, 4), ci.ub = round(x$ci.ub, 4), L.0 = round(x$L.0, 4), pval = x$pval.0, ksig = x$ksig, row.names = ""), width = 9))
     cat("\n")
   }
   if (x$method == "KS" | x$method == "AD") {
@@ -83,9 +84,12 @@ print.puniformoutput <- function(x) {
   cat("Publication bias test p-uniform")
   cat("\n")
   cat("\n")
-  if(x$method == "LNP" | x$method == "LN1MINP") { print(format(data.frame(L.pb = round(x$L.pb, 4), pval = round(x$pval.pb, 4), row.names = ""), width = 9))
+  if(x$method == "LNP" | x$method == "LN1MINP") { 
+    x$pval.pb <- ifelse(x$pval.pb < .001, "  <.001", round(x$pval.pb, 4))
+    print(format(data.frame(L.pb = round(x$L.pb, 4), pval = x$pval.pb, row.names = ""), width = 9))
   } else if(x$method == "P") {
-    print(format(data.frame(z.pb = round(x$L.pb, 4), pval = round(x$pval.pb, 4), row.names = ""), width = 9))
+    x$pval.pb <- ifelse(x$pval.pb < .001, "  <.001", round(x$pval.pb, 4))
+    print(format(data.frame(z.pb = round(x$L.pb, 4), pval = x$pval.pb, row.names = ""), width = 9))
     cat("\n")
     cat("p-value approximated with normal distribution")
     cat("\n")
@@ -103,8 +107,10 @@ print.puniformoutput <- function(x) {
   cat("Fixed-effect meta-analysis")
   cat("\n")
   cat("\n")
+  x$Qpval <- ifelse(x$Qpval < .001, "  <.001", round(x$Qpval, 4))
+  x$pval.fe <- ifelse(x$pval.fe < .001, "  <.001", round(x$pval.fe, 4))
   print(format(data.frame(est.fe = round(x$est.fe, 4), se.fe = round(x$se.fe, 4), zval.fe = round(x$zval.fe, 4),
-                          pval.fe = round(x$pval.fe, 4), ci.lb.fe = round(x$ci.lb.fe, 4), ci.ub.fe = round(x$ci.ub.fe, 4),
-                          Qstat = round(x$Qstat, 4), Qpval = round(x$Qpval, 4), row.names = ""), width = 9))
+                          pval.fe = x$pval.fe, ci.lb.fe = round(x$ci.lb.fe, 4), ci.ub.fe = round(x$ci.ub.fe, 4),
+                          Qstat = round(x$Qstat, 4), Qpval = x$Qpval, row.names = ""), width = 9))
   cat("\n")
 }
